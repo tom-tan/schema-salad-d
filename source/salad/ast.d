d05f65c074efa8fb0d36b3c93bd8976976b4ace2
Tomoya Tanjo <ttanjo@gmail.com>
2021-08-04 07:06:01 +0000

    Add edig for AST type

--- source/salad/ast.d
+++ source/salad/ast.d
@@ -72,10 +72,9 @@ AST dig(T)(AST ast, string[] keys, T default_)
         return ast;
     }
 
-    AST ret = ast;
     auto k = keys[0];
 
-    auto rec = ret.value.tryMatch!((RecordType rt) => rt);
+    auto rec = ast.value.tryMatch!((RecordType rt) => rt);
     if (k.canFind("://"))
     {
         if (auto a = k in rec.extensionFields)
@@ -111,5 +110,54 @@ AST dig(T)(AST ast, string[] keys, T default_)
         {
             return new AST(Node.init, "Any", default_);
         }
+    }
+}
+
+/// edig
+auto edig(Ex = Exception)(AST ast, string key)
+{
+    return edig!Ex(ast, [key]);
+}
+
+/// ditto
+AST edig(Ex = Exception)(AST ast, string[] keys)
+{
+    import std.algorithm : canFind;
+    import std.range : empty;
+
+    if (auto a = ast.value.match!((AST a) => a, others => null))
+    {
+        return a.edig!Ex(keys);
+    }
+    else if (keys.empty)
+    {
+        return ast;
+    }
+
+    auto k = keys[0];
+
+    auto rec = ast.value.tryMatch!((RecordType rt) => rt);
+    if (k.canFind("://"))
+    {
+        if (auto a = k in rec.extensionFields)
+        {
+            import salad.util : edig;
+            auto n = a.value.tryMatch!((Node n) => n.edig!Ex(keys[1..$]));
+            return new AST(n, "Any", n);
+        }
+        else
+        {
+            import std.format : format;
+            throw new Ex(format!"No such field: %s"(k));
+        }
+    }
+    else if (auto a = k in rec.fields)
+    {
+        return (*a).edig!Ex(keys[1..$]);
+    }
+    else
+    {
+        import std.format : format;
+        throw new Ex(format!"No such field: %s"(k));
     }
 }
