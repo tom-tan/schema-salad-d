008172b4c838b6ff001497b07cd751775fe417ed
Tomoya Tanjo <ttanjo@gmail.com>
2022-12-11 01:37:36 +0000

    Deprecate hand-written CWL parser

--- source/cwl/package.d
+++
@@ -1,8 +0,0 @@
-/**
- * Authors: Tomoya Tanjo
- * Copyright: © 2021 Tomoya Tanjo
- * License: Apache-2.0
- */
-module cwl;
-
-public import cwl.v1_0;
--- source/cwl/v1_0.d
+++ source/cwl/v1_0.d
@@ -1,14 +1,18 @@
 /**
  * Hand-written definition of CWL v1.0
+ * It is used only for testing.
  *
  * Authors: Tomoya Tanjo
  * Copyright: © 2021 Tomoya Tanjo
  * License: Apache-2.0
  */
-module cwl.v1_0.schema;
+module cwl.v1_0;
+
+version(unittest):
 
 import salad.meta.dumper : genDumper;
 import salad.meta.impl : genCtor, genIdentifier, genOpEq;
+import salad.meta.parser : import_ = importFromURI;
 import salad.meta.uda : documentRoot, id, idMap, link, LinkResolver, typeDSL;
 import salad.primitives : SchemaBase;
 import salad.type : Either, Optional;
@@ -1033,4 +1037,166 @@ unittest
     assert(wf.dig!"cwlVersion"(new CWLVersion("draft-3")) == "v1.0");
     assert(wf.dig!(["inputs", "file1", "type"], CWLType) == "File");
     assert(wf.dig!(["outputs", "count_output", "outputSource"], string) == "step2/output");
+}
+
+///
+alias DocumentRootType = Either!(CommandLineTool, ExpressionTool, Workflow);
+
+///
+alias importFromURI = import_!DocumentRootType;
+
+///
+@safe unittest
+{
+    import core.exception : AssertError;
+    import dyaml : Node;
+    import salad.resolver : absoluteURI;
+    import salad.type : tryMatch;
+    import salad.util : dig, edig;
+    import std.exception : assertNotThrown, enforce;
+
+    auto uri = "examples/cwl-v1.0/bwa-mem-tool.cwl".absoluteURI;
+
+    auto cwl = importFromURI(uri).tryMatch!((DocumentRootType r) => r)
+                                 .assertNotThrown;
+    assert(cwl.edig!("class", string) == "CommandLineTool");
+
+    auto cmd = cwl.tryMatch!((CommandLineTool c) => c)
+                  .assertNotThrown;
+    assert(cmd.dig!"cwlVersion"(new CWLVersion("draft-2")) == "v1.0");
+    assert(cmd.dig!(["inputs", "reference", "type"], CWLType) == "File");
+    assert(cmd.dig!(["hints", "ResourceRequirement"], ResourceRequirement)
+              .enforce!AssertError
+              .dig!("coresMin", long) == 2);
+    assert(cmd.edig!(["hints", "ResourceRequirement"], ResourceRequirement)
+              .assertNotThrown
+              .dig!("coresMin", long) == 2);
+
+    auto node = Node(cmd);
+}
+
+///
+@safe unittest
+{
+    import salad.resolver : absoluteURI;
+    import salad.type : tryMatch;
+    import salad.util : dig, edig;
+    import std.exception : assertNotThrown;
+
+    auto uri = "examples/cwl-v1.0/count-lines1-wf.cwl".absoluteURI;
+    auto cwl = importFromURI(uri).tryMatch!((DocumentRootType r) => r)
+                                 .assertNotThrown;
+    assert(cwl.edig!("class", string) == "Workflow");
+
+    auto wf = cwl.tryMatch!((Workflow w) => w)
+                 .assertNotThrown;
+    assert(wf.edig!("class", string) == "Workflow");
+    assert(wf.dig!"cwlVersion"(new CWLVersion("draft-3")) == "v1.0");
+    assert(wf.dig!(["inputs", "file1", "type"], CWLType) == "File");
+    assert(wf.dig!(["outputs", "count_output", "outputSource"], string) == "step2/output");
+}
+
+@safe unittest
+{
+    import salad.resolver : absoluteURI;
+    import salad.type : tryMatch;
+    import salad.util : edig;
+    import std.exception : assertNotThrown;
+
+    auto uri = "examples/cwl-v1.0/revsort-packed.cwl".absoluteURI;
+    auto cwls = importFromURI(uri).tryMatch!((DocumentRootType[] rs) => rs)
+                                  .assertNotThrown;
+    assert(cwls.length == 3);
+
+    {
+        auto cwl = importFromURI(uri, "#main").tryMatch!((DocumentRootType r) => r)
+                                              .assertNotThrown;
+        assert(cwl.edig!("class", string) == "Workflow");
+    }
+
+    {
+        auto cwl = importFromURI(uri, "main").tryMatch!((DocumentRootType r) => r)
+                                             .assertNotThrown;
+        assert(cwl.edig!("class", string) == "Workflow");
+
+        auto wf = cwl.tryMatch!((Workflow w) => w)
+                     .assertNotThrown;
+        assert(wf.edig!("doc", string) == "Reverse the lines in a document, then sort those lines.");
+    }
+}
+
+@safe unittest
+{
+    import dyaml : Loader;
+    import salad.util : edig;
+
+    enum cwl = "examples/cwl-v1.0/glob-expr-list.cwl";
+    auto clt = Loader.fromFile(cwl)
+                     .load
+                     .as!CommandLineTool;
+    assert(clt.edig!(["inputs", "ids", "type", "type"], string) == "array");
+}
+
+@safe unittest
+{
+    import salad.resolver : absoluteURI;
+    import salad.type : tryMatch;
+    import salad.util : edig;
+    import std.exception : assertNotThrown;
+
+    auto uri = "examples/cwl-v1.0/params.cwl".absoluteURI;
+
+    auto cwl = importFromURI(uri).tryMatch!((DocumentRootType r) => r)
+                                 .assertNotThrown;
+    assert(cwl.edig!("class", string) == "CommandLineTool");
+
+    auto cmd = cwl.tryMatch!((CommandLineTool c) => c)
+                  .assertNotThrown;
+    assert(cmd.edig!(["outputs", "t1", "type"], string) == "Any");
+}
+
+@safe unittest
+{
+    import salad.resolver : absoluteURI;
+    import salad.type : tryMatch;
+    import salad.util : edig;
+    import std.exception : assertNotThrown;
+
+    auto uri = "examples/cwl-v1.0/formattest.cwl".absoluteURI;
+
+    auto cmd = importFromURI(uri).tryMatch!((DocumentRootType r) => r)
+                                 .tryMatch!((CommandLineTool c) => c)
+                                 .assertNotThrown;
+    assert(cmd.edig!(["inputs", "input", "format"], string) == "http://edamontology.org/format_2330");
+}
+
+@safe unittest
+{
+    import salad.resolver : absoluteURI;
+    import salad.type : tryMatch;
+    import salad.util : edig;
+    import std.exception : assertNotThrown;
+
+    auto uri = "examples/cwl-v1.0/formattest2.cwl".absoluteURI;
+
+    auto cmd = importFromURI(uri).tryMatch!((DocumentRootType r) => r)
+                                 .tryMatch!((CommandLineTool c) => c)
+                                 .assertNotThrown;
+    assert(cmd.edig!(["outputs", "output", "format"], string) == "$(inputs.input.format)");
+    assert(cmd.identifier == uri);
+}
+
+@safe unittest
+{
+    import salad.resolver : absoluteURI;
+    import salad.type : tryMatch;
+    import salad.util : edig;
+    import std.exception : assertNotThrown;
+
+    auto uri = "examples/cwl-v1.0/search.cwl".absoluteURI;
+
+    auto wf = importFromURI(uri~"#main").tryMatch!((DocumentRootType r) => r)
+                                 .tryMatch!((Workflow wf) => wf)
+                                 .assertNotThrown;
+    assert(wf.identifier == uri~"#main");
 }
